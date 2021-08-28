4d94f94f5f8a8c14cea11d1660c8c7cbb8cf081e
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-08-28 09:28:44 -0700

    Add files via upload
    first four queries completed

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,35 @@
+SELECT employees.emp_no, 
+		employees.last_name, 
+		employees.first_name,
+		employees.sex,
+		salaries.salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no = salaries.emp_no;
+
+SELECT first_name,
+		last_name,
+		hire_date
+FROM EMPLOYEES
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+SELECT employees.last_name,
+	employees.first_name,
+	dept_manager.emp_no,
+	dept_manager.dept_no,
+	departments.dept_name
+FROM departments
+INNER JOIN dept_manager ON
+dept_manager.dept_no=departments.dept_no
+INNER JOIN employees ON
+dept_manager.emp_no=employees.emp_no;
+
+,SELECT employees.last_name,
+	employees.first_name,
+	dept_emp.emp_no,
+	departments.dept_name
+FROM departments
+INNER JOIN dept_emp ON
+dept_emp.dept_no=departments.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no=employees.emp_no, 
