cb8624442bf4d3725b875f195155853ef9e2f499
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-08-28 12:18:38 -0700

    Add files via upload
    All queries completed and formatted with a description of what each query performs.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,3 +1,5 @@
+--#1. List the following details of each employee:
+--employee number, last name, first name, sex, and salary.
 SELECT employees.emp_no, 
 		employees.last_name, 
 		employees.first_name,
@@ -7,12 +9,15 @@ FROM salaries
 INNER JOIN employees ON
 employees.emp_no = salaries.emp_no;
 
+--2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name,
 		last_name,
 		hire_date
 FROM EMPLOYEES
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
+--#3. List the manager of each department with the following information:
+--department number, department name, the manager’s employee number, last name, first name.
 SELECT employees.last_name,
 	employees.first_name,
 	dept_manager.emp_no,
@@ -24,6 +29,8 @@ dept_manager.dept_no=departments.dept_no
 INNER JOIN employees ON
 dept_manager.emp_no=employees.emp_no;
 
+--4 List the department of each employee with the following information:
+--employee number, last name, first name, and department name.
 SELECT employees.last_name,
 	employees.first_name,
 	dept_emp.emp_no,
@@ -32,7 +39,7 @@ FROM departments
 INNER JOIN dept_emp ON
 dept_emp.dept_no=departments.dept_no
 INNER JOIN employees ON
-dept_emp.emp_no=employees.emp_no, 
+dept_emp.emp_no=employees.emp_no; 
 
 --#5 List first name, last name, and sex for employees whose first name
 --#is “Hercules” and last names begin with “B.”
@@ -53,7 +60,7 @@ INNER JOIN employees ON
 dept_emp.emp_no=employees.emp_no
 WHERE departments.dept_name LIKE '%Sales%'
 
---7 List all employees in the Sales and Development departments,
+--#7 List all employees in the Sales and Development departments,
 --including their employee number, last name, first name, and department name.
 SELECT dept_emp.emp_no,
 		employees.last_name,
@@ -66,3 +73,10 @@ INNER JOIN employees ON
 dept_emp.emp_no=employees.emp_no
 WHERE departments.dept_name LIKE '%Sales%' 
 OR departments.dept_name LIKE '%Development%'
+
+--#8 In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name)
+FROM EMPLOYEES
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC
